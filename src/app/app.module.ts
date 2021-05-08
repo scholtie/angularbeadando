098d0fab72cbf6ab374358f7b0e40fdb7ea412e7
Daniel Solti <solti.dani7@gmail.com>
2021-05-08 19:22:27 +0200

    basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { FormComponent } from './form/form.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    FormComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
